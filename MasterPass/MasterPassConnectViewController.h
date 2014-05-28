f0a20eb6ecd6aace6a37f48498cd0d1d2fd0f213
David Benko <dbenko@anypresence.com>
2014-05-28 13:00:12 -0400

    Tap to dismiss last mc connectg webpage

--- MasterPass/MasterPassConnectViewController.h
+++ MasterPass/MasterPassConnectViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MasterPassConnectViewController : UIViewController
+@interface MasterPassConnectViewController : UIViewController <UIWebViewDelegate>
 @property(nonatomic, strong)NSString *path;
 @end
--- MasterPass/MasterPassConnectViewController.m
+++ MasterPass/MasterPassConnectViewController.m
@@ -13,9 +13,12 @@
 @end
 
 @implementation MasterPassConnectViewController
+
+#pragma mark - UIViewController methods
 -(void)viewDidAppear:(BOOL)animated{
     [super viewDidAppear:animated];
-    
+    self.webview.delegate = self;
+    self.webview.userInteractionEnabled = YES;
     NSURLRequest *request = [[NSURLRequest alloc]initWithURL:[NSURL fileURLWithPath:self.path]];
     [self.webview loadRequest:request];
 }
@@ -28,5 +31,39 @@
 }
 -(IBAction)cancel{
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - UIWebViewDelegate Methods
+- (void)webViewDidFinishLoad:(UIWebView *)webview{
+    
+    /*
+     * This is a hack to allow the user to tap the webview on the last 
+     * page and close it. This functionality will be replaced with real
+     * SDK methods, when those are available, which will hopefully
+     * be using some sort of Obj-C/Javascript triggering.
+     *
+     */
+    
+    NSString *currentUrl = webview.request.mainDocumentURL.absoluteString;
+    
+    NSArray *docs = @[@"mp3",@"mp4-express",@"mp5-express-checkout",@"mp4-checkout"]; // these are the end pages
+    NSMutableArray *paths = [[NSMutableArray alloc]init];
+    
+    [docs each:^(NSString * doc) {
+        [paths addObject:[[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:doc ofType:@"html"]] absoluteString]];
+    }];
+    
+    if ([paths includes:currentUrl]) {
+        // We are on an end page
+        UIView *tapView = [[UIView alloc]initWithFrame:CGRectZero];
+        [self.view addSubview:tapView];
+        [tapView makeConstraints:^(MASConstraintMaker *make) {
+            make.edges.equalTo(self.view);
+        }];
+        
+        [tapView bk_whenTapped:^{
+            [self done];
+        }];
+    }
 }
 @end
