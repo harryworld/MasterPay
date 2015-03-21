4c2c7f28d537d4cb11a3405810945399bcdb79df
Harry Ng <harryworld@gmail.com>
2015-03-21 22:54:00 +0800

    Connect rePower to API

--- MasterPass/RepowerViewController.m
+++ MasterPass/RepowerViewController.m
@@ -21,9 +21,58 @@
 
 - (IBAction)submitButtonClicked:(id)sender
 {
-    if ([self.amountToBeAdded.text isEqualToString:@""]) return;
+    [self loadData];
+}
+
+- (void)loadData
+{
+    [self.amountToBeAdded resignFirstResponder];
+    [self.codeToBeUsed resignFirstResponder];
     
-    self.balance.text = self.amountToBeAdded.text;
+    NSString *serverAddress = [NSString stringWithFormat:@"http://desolate-river-6178.herokuapp.com"];
+    
+    NSString *amount = self.amountToBeAdded.text;
+    if ([amount isEqualToString:@""]) {
+        amount = @"500";
+    }
+    self.amountToBeAdded.text = @"";
+    
+    NSString *cardNumber = self.codeToBeUsed.text;
+    if ([cardNumber isEqualToString:@""]) {
+        cardNumber = @"5204740009900014";
+    }
+    self.codeToBeUsed.text = @"";
+    
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@/repower/%@/%@", serverAddress, amount, cardNumber]];
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:@"GET"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    [NSURLConnection sendAsynchronousRequest:request queue:queue completionHandler:^(NSURLResponse *response, NSData *data, NSError *error){
+        
+        if (error) {
+            NSLog(@"error: %@", error);
+        }
+        else{
+            NSError * jsonError;
+            NSDictionary* json = [NSJSONSerialization JSONObjectWithData:data
+                                                                 options:kNilOptions
+                                                                   error:&jsonError];
+            if (jsonError) {
+                NSLog(@"error: %@", jsonError);
+            }
+            else {
+                self.balance.text = [json objectForKey:@"value"];
+                [MBProgressHUD hideHUDForView:self.view animated:YES];
+           }
+            
+        }
+    }];
 }
 
 @end
